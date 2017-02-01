0d39702355aec19d33e83fd2e9be4268a4285f4f
alumno1 <alumno@macbook04.local>
2017-01-31 21:17:37 -0500

    Cuarta Clase de IOS

--- clase 4 Navegacion en IOS/ViewController.swift
+++ clase 4 Navegacion en IOS/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnNavegationCodigo(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "transition", sender: sender)
+    }
 
 }
 
